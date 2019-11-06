c1dc58cdc807dbb81132e1dce2896e4ffbe8f50f
Tor Solli-Nowlan <tor.solli-nowlan@medisin.uio.no>
2019-11-06 08:36:49 +0000

    move to python3, change folders_to_extract

--- unpack_lfs.py
+++ unpack_lfs.py
@@ -1,76 +1,66 @@
-from __future__ import print_function
-import subprocess
+#!/usr/bin/env python3
+
 import os
+from pathlib import Path
+import subprocess
 
 
 def unpack_folder(archive):
     print("Processing archive {}".format(archive))
-    if not os.path.isfile(archive):
+    if not archive.exists():
         print("Archive {} does not exist. Aborting.".format(archive))
         return
 
-    target, ext = archive.split(".", 1)
-    directory, archive_file = archive.rsplit("/", 1)
+    target, ext = str(archive).split(".", 1)
+    directory = archive.parent
+    archive_file = archive.name
 
     if ext not in ["tar", "tar.gz"]:
         print("Not an archive ({}). Aborting.".format(archive))
 
-    if os.path.isdir(target):
+    if Path(target).is_dir():
         print("Target directory {} exists. Assuming archive extracted.".format(target))
         return
     try:
         subprocess.check_call(
-            "cd {}; tar --no-same-owner --no-same-permissions -xf {}".format(
-                directory, archive_file
-            ),
-            shell=True,
+            "tar --no-same-owner --no-same-permissions -xf {}".format(archive_file), cwd=directory, shell=True
         )
     except:
-        subprocess.call("cd {}; rm -rf {}".format(directory, target), shell=True)
+        subprocess.call("rm -rf {}".format(target), cwd=directory, shell=True)
         raise
 
 
 def unpack_file(archive):
-    if not os.path.isfile(archive):
+    if not archive.is_file():
         print("Archive {} does not exist. Aborting.".format(archive))
         return
 
-    directory, archive_file = archive.rsplit("/", 1)
-    target, ext = archive_file.rsplit(".", 1)
+    target, ext = str(archive).split(".", 1)
+    directory = archive.parent
+    archive_file = archive.name
 
     if ext != "gz":
         print("Not an archive ({}). Aborting.".format(archive))
         return
 
-    target_path = os.path.join(directory, target)
-    if os.path.isfile(target_path):
-        print(
-            "Target file exists ({}). Assuming archive extracted.".format(target_path)
-        )
+    target_path = directory / target
+    if target_path.is_file():
+        print("Target file exists ({}). Assuming archive extracted.".format(target_path))
         return
     try:
-        subprocess.check_call(
-            "cd {}; zcat {} > {}".format(directory, archive_file, target), shell=True
-        )
+        subprocess.check_call("zcat {} > {}".format(archive_file, target), cwd=directory, shell=True)
     except:
-        subprocess.call("cd {}; rm -f {}".format(directory, target), shell=True)
+        subprocess.call("rm -f {}".format(target), cwd=directory, shell=True)
         raise
 
 
 if __name__ == "__main__":
-    abspath = os.path.abspath(__file__)
-    dname = os.path.dirname(abspath)
+    abspath = Path(__file__).absolute()
+    dname = abspath.parent
     os.chdir(dname)
 
-    folders_to_extract = [
-        "data/VEP/cache.tar",
-        "data/seqrepo.tar",
-        "thirdparty/vep.tar.gz",
-        "thirdparty/bedtools2.tar.gz",
-        "thirdparty/vcftools.tar.gz",
-    ]
-
-    files_to_extract = ["data/FASTA/human_g1k_v37_decoy.fasta.gz"]
+    folders_to_extract = [Path("data.tar").absolute(), Path("thirdparty.tar.gz").absolute()]
+    files_to_extract = [Path("data/FASTA/human_g1k_v37_decoy.fasta.gz").absolute()]
 
     for archive in folders_to_extract:
         unpack_folder(archive)
