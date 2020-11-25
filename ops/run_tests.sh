da94ce2aaa6f89f2d46a4ad0128bf89068283eea
Øyvind Evju <oyvind.evju@medisin.uio.no>
2020-11-25 14:45:10 +0100

    Move UTA to datasets.json/DigitalOcean. Dynamically fetch UTA_DB_URL from code. Create sourceme file for postgres functions.

--- ops/run_tests.sh
+++ ops/run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
 /anno/ops/pg_startup &
-
-while ! pg_isready --host=$PGDATA --dbname=postgres --username=postgres; do sleep 5; done
+/anno/ops/pg_wait 5 30
 
 VERBOSE=0 SAMPLES=/anno/tests/testdata/sample_repo TARGETS=/anno/tests/testdata/targets python -m pytest /anno/tests/ --ignore /anno/tests/opstests -sv
 
 /anno/ops/pg_shutdown
+
