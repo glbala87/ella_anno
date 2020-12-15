252a6a66563c15e65e5e41886aa3b1643f49d845
Øyvind Evju <oyvind.evju@medisin.uio.no>
2020-12-15 10:28:59 +0100

    Only trim alt alleles where ALT contains the star allele, leave everything else untouched

--- src/annotation/remove_star_genotypes.py
+++ src/annotation/remove_star_genotypes.py
@@ -17,49 +17,52 @@ def order_genotype(gt):
     return f"{min(gt0,gt1)}/{max(gt0,gt1)}"
 
 
-def remove_star_alleles(input):
-    with open(input) as f:
-        it = iter(f)
+def remove_star_alleles(f):
+    it = iter(f)
 
-        # Iterate over header
-        for l in it:
-            print(l.rstrip("\n"))
-            if l.startswith("#CHROM"):
-                break
+    # Iterate over header
+    for l in it:
+        print(l.rstrip("\n"))
+        if l.startswith("#CHROM"):
+            break
 
-        # Iterate over body
-        for line in it:
-            columns = line.split("\t")
-            alts = columns[4].split(",")
-            if "*" not in alts:
-                print(line)
-                continue
+    # Iterate over body
+    for line in it:
+        columns = line.split("\t")
+        alts = columns[4].split(",")
+        if "*" not in alts:
+            print(line)
+            continue
 
-            # We fetch the allele index here to modify the genotype only
-            # Leave the removal of the star allele to bcf-tools
-            allele_index = alts.index("*")
-            fmat = columns[8].split(":")
-            if "GT" not in fmat:
-                # This shouldn't happen, but leave it to other tools to handle
-                print(line)
-                continue
+        # We fetch the allele index here to modify the genotype only
+        # Leave the removal of the star allele to bcf-tools
+        allele_index = alts.index("*")
+        fmat = columns[8].split(":")
+        if "GT" not in fmat:
+            # This shouldn't happen, but leave it to other tools to handle
+            print(line)
+            continue
 
-            gt_index = fmat.index("GT")
-            for i in range(9, len(columns)):
-                fmat_data = columns[i].rstrip("\n").split(":")
+        gt_index = fmat.index("GT")
+        for i in range(9, len(columns)):
+            fmat_data = columns[i].rstrip("\n").split(":")
 
-                # Change genotype referring to star allele to refer to reference instead.
-                # Note: This is not correct, but the overlapping deletion should also be in the VCF with a complementing genotype
-                gt_removed_star_allele = re.sub(str(allele_index + 1), "0", fmat_data[gt_index])
+            # Change genotype referring to star allele to refer to reference instead.
+            # Note: This is not correct, but the overlapping deletion should also be in the VCF with a complementing genotype
+            gt_removed_star_allele = re.sub(str(allele_index + 1), "0", fmat_data[gt_index])
 
-                # Order genotype (e.g. 1/0 -> 0/1)
-                fmat_data[gt_index] = order_genotype(gt_removed_star_allele)
-                columns[i] = ":".join(fmat_data)
+            # Order genotype (e.g. 1/0 -> 0/1)
+            fmat_data[gt_index] = order_genotype(gt_removed_star_allele)
+            columns[i] = ":".join(fmat_data)
 
-            # Print out fixed line
-            print("\t".join(columns))
+        # Print out fixed line
+        print("\t".join(columns))
 
 
 if __name__ == "__main__":
-    input = sys.argv[1]
+    if len(sys.argv) > 1:
+        input = open(sys.argv[1], "r")
+    else:
+        input = sys.stdin
+
     remove_star_alleles(input)
