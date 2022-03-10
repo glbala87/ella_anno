065780ebbc20d21b62302076bf4328852b2806c9
xuyangy <xuyang.yuan@medisin.uio.no>
2022-03-10 12:35:05 +0100

    fix ci

--- ops/run_tests.sh
+++ ops/run_tests.sh
@@ -3,6 +3,6 @@
 /anno/ops/pg_startup &
 /anno/ops/pg_wait 5 60
 
-VERBOSE=0 SAMPLES=/anno/tests/testdata/sample_repo TARGETS=/anno/tests/testdata/targets python3 -m pytest /anno/tests/ --ignore /anno/tests/opstests -sv
+VERBOSE=0 SAMPLES=/anno/tests/testdata/sample_repo TARGETS=/anno/tests/testdata/targets ANNO_CONFIG_PATH=/anno/tests/testdata/anno_global_config.json python3 -m pytest /anno/tests/ --ignore /anno/tests/opstests -sv
 
 /anno/ops/pg_shutdown
