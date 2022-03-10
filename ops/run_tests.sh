3c6089e1c3ada7c05557f957439bb596f7790b86
xuyangy <xuyang.yuan@medisin.uio.no>
2022-03-10 16:49:58 +0100

    fix ci

--- ops/run_tests.sh
+++ ops/run_tests.sh
@@ -3,6 +3,14 @@
 /anno/ops/pg_startup &
 /anno/ops/pg_wait 5 60
 
-VERBOSE=0 SAMPLES=/anno/tests/testdata/sample_repo TARGETS=/anno/tests/testdata/targets ANNO_CONFIG_PATH=/anno/tests/testdata/anno_global_config.json python3 -m pytest /anno/tests/ --ignore /anno/tests/opstests -sv
+# env vars required by anno config parser
+export SAMPLE_ID="Diag-wgs1-NA12878"
+export GP_NAME="Ciliopati"
+export GP_VERSION="v06"
+export TYPE="single"
+export CAPTUREKIT="wgs"
+export ANNO_CONFIG_PATH=/anno/tests/testdata/anno_global_config.json
+
+VERBOSE=0 SAMPLES=/anno/tests/testdata/sample_repo TARGETS=/anno/tests/testdata/targets  python3 -m pytest /anno/tests/ --ignore /anno/tests/opstests -sv
 
 /anno/ops/pg_shutdown
